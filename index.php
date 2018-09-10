8db7603bf68ad850486983e55bde870a5f6e1745
Gertin Pakkonen <gertpak@users.no-reply.github.com>
2018-09-10 10:50:43 +0300

    html tehtud phpks

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Gertini veebileht</title>
+
+</head>
+<body>
+	<h1>Gertin Pakkonen</h1>
+	<p>Siin on minu <a href="http://tlu.ee/" target="_blank">TLÜ</a> õppetöö raames valminud veebilehed. Need ei oma mingit sügavat sisu ja nende kopeerimine ei oma mõtet.</p>
+	<p>Kodutöö on ilusti tehtud</p>  
+	<p>Mu kõrval istub <a href="../../~sandhan/" target="_blank">Sander</a>.</p>
+	<!--<img src="tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone"> -->
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+	<img src="tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">	 -->
+	<img src="../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+	<img src="https://i.imgflip.com/u9pv5.jpg" alt="MEME">
+	 
+	
+	 
+</body>
+
+</html>
